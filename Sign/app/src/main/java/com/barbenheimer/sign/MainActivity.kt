8bd3196ebbed5fba5a4a4660fe404dfafde32d35
deloreancodes <loben_klien_tipan@dlsu.edu.ph>
2023-09-20 20:15:45 +0800

    added clear button again

--- Sign/app/src/main/java/com/barbenheimer/sign/MainActivity.kt
+++ Sign/app/src/main/java/com/barbenheimer/sign/MainActivity.kt
@@ -1,21 +1,18 @@
 
 package com.barbenheimer.sign
 
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.android.volley.Request
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 
 import com.barbenheimer.sign.MainViewModel
 import com.barbenheimer.sign.databinding.ActivityMainBinding
-import com.barbenheimer.sign.fragment.CameraFragment
-import org.json.JSONObject
+import com.barbenheimer.sign.fragment.
+import android.view.View
+
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var activityMainBinding: ActivityMainBinding
@@ -30,80 +27,14 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.fragment_container, cameraFragment)
         fragmentTransaction.addToBackStack(null) // Optional: Adds the transaction to the back stack
         fragmentTransaction.commit()
-        val jsonObject = JSONObject()
-        jsonObject.put("data", "WOW")
-        val volleyQueue = Volley.newRequestQueue(this)
-        val url = "http://10.0.2.2:5000/pose"
-        val jsonObjectRequest = JsonObjectRequest(
-            // we are using GET HTTP request method
-            Request.Method.POST,
-            // url we want to send the HTTP request to
-            url,
-            // this parameter is used to send a JSON object
-            // to the server, since this is not required in
-            // our case, we are keeping it `null`
-            jsonObject,
 
-            // lambda function for handling the case
-            // when the HTTP request succeeds
-            { response ->
-                // get the image url from the JSON object
-                val msg = response.get("pose_data")
-                //TODO: handle displaying of translations on screen
-                Log.d("RES MSG", "message: " + msg)
-                // load the image into the ImageView using Glide.
+        // Clear text from TextView
+        val clearButton = findViewById<Button>(R.id.clearButton)
+        val translationText = findViewById<TextView>(R.id.translationText) // Initialize TextView
 
-            },
+        clearButton.setOnClickListener {
+            translationText.text = "" // Clear the text in the TextView
 
-            // lambda function for handling the
-            // case when the HTTP request fails
-            { error ->
-                // make a Toast telling the user
-                // that something went wrong
-
-                // log the error message in the error stream
-                Log.e("MainActivity", "loadDogImage error: ${error.message}")
-            }
-        )
-        volleyQueue.add(jsonObjectRequest)
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_NETWORK_STATE)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission not granted, request it
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.ACCESS_NETWORK_STATE),
-                123
-            )
-        }
-
-        // Check for INTERNET permission
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.INTERNET)
-            != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Permission not granted, request it
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(android.Manifest.permission.INTERNET),
-                123
-            )
-        }
-    }
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 123) {
-            // Check if permissions were granted
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission granted, you can now use the permission
-            } else {
-                // Permission denied, handle accordingly
-            }
-        }
-    }
     override fun onBackPressed() {
        finish()
     }
