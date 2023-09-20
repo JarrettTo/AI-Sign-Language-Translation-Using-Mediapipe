3474088781f3e0e8ee61257f96ec90006a840576
andres_dalisay <andres_dalisay@dlsu.edu.ph>
2023-09-21 03:27:33 +0800

    fixed skeleton not showing

--- Sign/app/src/main/java/com/barbenheimer/sign/fragment/CameraFragment.kt
+++ Sign/app/src/main/java/com/barbenheimer/sign/fragment/CameraFragment.kt
@@ -1,9 +1,11 @@
 package com.barbenheimer.sign.fragment
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -26,6 +28,8 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
@@ -36,24 +40,88 @@ import androidx.camera.video.QualitySelector
 import androidx.camera.video.VideoRecordEvent
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.barbenheimer.sign.MainActivity
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.Locale
 import com.barbenheimer.sign.HandLandmarkerHelper
+import com.barbenheimer.sign.MainViewModel
 import com.barbenheimer.sign.R
 import com.google.mediapipe.tasks.vision.core.RunningMode
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.concurrent.TimeUnit
 
 typealias LumaListener = (luma: Double) -> Unit
 
 class CameraFragment : Fragment(), HandLandmarkerHelper.LandmarkerListener {
-    
+
     private lateinit var cameraExecutor: ExecutorService
 
     private var _fragmentCameraBinding: FragmentCameraBinding? = null
     private lateinit var backgroundExecutor: ExecutorService
     private val fragmentCameraBinding
         get() = _fragmentCameraBinding!!
+
+    var transmit = false
+
+    private lateinit var handLandmarkerHelper: HandLandmarkerHelper
+    private val viewModel: MainViewModel by activityViewModels()
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var cameraFacing = CameraSelector.LENS_FACING_FRONT
+    private var poseArray : ArrayList<FloatArray> = ArrayList()
+
+    override fun onResume() {
+        super.onResume()
+//        // Make sure that all permissions are still present, since the
+//        // user could have removed them while the app was in paused state.
+//        if (!PermissionsFragment.hasPermissions(requireContext())) {
+//            Navigation.findNavController(
+//                requireActivity(), R.id.fragment_container
+//            ).navigate(R.id.action_camera_to_permissions)
+//        }
+
+        // Start the HandLandmarkerHelper again when users come back
+        // to the foreground.
+        backgroundExecutor.execute {
+            if (handLandmarkerHelper.isClose()) {
+                handLandmarkerHelper.setupHandLandmarker()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(this::handLandmarkerHelper.isInitialized) {
+            viewModel.setMaxHands(handLandmarkerHelper.maxNumHands)
+            viewModel.setMinHandDetectionConfidence(handLandmarkerHelper.minHandDetectionConfidence)
+            viewModel.setMinHandTrackingConfidence(handLandmarkerHelper.minHandTrackingConfidence)
+            viewModel.setMinHandPresenceConfidence(handLandmarkerHelper.minHandPresenceConfidence)
+            viewModel.setDelegate(handLandmarkerHelper.currentDelegate)
+
+            // Close the HandLandmarkerHelper and release resources
+            backgroundExecutor.execute { handLandmarkerHelper.clearHandLandmarker() }
+        }
+    }
+
+    override fun onDestroyView() {
+        _fragmentCameraBinding = null
+        super.onDestroyView()
+
+        // Shut down our background executor
+        backgroundExecutor.shutdown()
+        backgroundExecutor.awaitTermination(
+            Long.MAX_VALUE, TimeUnit.NANOSECONDS
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,6 +130,25 @@ class CameraFragment : Fragment(), HandLandmarkerHelper.LandmarkerListener {
     ): View? {
         _fragmentCameraBinding =
             FragmentCameraBinding.inflate(inflater, container, false)
+        val recordButton = _fragmentCameraBinding!!.button2
+        recordButton.setOnClickListener{
+            transmit = !transmit
+            if (transmit) {
+                recordButton.text = "stop"
+                recordButton.setBackgroundColor(
+                    ContextCompat.getColor(requireContext(), R.color.mp_color_error)
+                )
+            } else {
+                recordButton.text = "Record"
+                recordButton.setBackgroundColor(
+                    ContextCompat.getColor(requireContext(), R.color.primary)
+                )
+                if(poseArray.size>=30 && !transmit){
+                    transmitPoints()
+                }
+            }
+            Log.d("TRANSMIT", "TRANSMIT" + transmit)
+        }
 
         return fragmentCameraBinding.root
 
@@ -80,52 +167,103 @@ class CameraFragment : Fragment(), HandLandmarkerHelper.LandmarkerListener {
         }
 
         // Create the HandLandmarkerHelper that will handle the inference
+        backgroundExecutor.execute {
+            handLandmarkerHelper = HandLandmarkerHelper(
+                context = requireContext(),
+                runningMode = RunningMode.LIVE_STREAM,
+                minHandDetectionConfidence = viewModel.currentMinHandDetectionConfidence,
+                minHandTrackingConfidence = viewModel.currentMinHandTrackingConfidence,
+                minHandPresenceConfidence = viewModel.currentMinHandPresenceConfidence,
+                maxNumHands = viewModel.currentMaxHands,
+                currentDelegate = viewModel.currentDelegate,
+                handLandmarkerHelperListener = this
+            )
+        }
+
     }
 
 
     private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        val cameraProviderFuture =
+            ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            {
+                // CameraProvider
+                cameraProvider = cameraProviderFuture.get()
 
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(_fragmentCameraBinding?.viewFinder?.surfaceProvider)
-                }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
-
-            } catch(exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(requireContext()))
+                // Build and bind the camera use cases
+                bindCameraUseCases()
+            }, ContextCompat.getMainExecutor(requireContext())
+        )
     }
-
-
-
-
 
     override fun onDestroy() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
 
+    // Declare and bind preview, capture and analysis use cases
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindCameraUseCases() {
+
+        // CameraProvider
+        val cameraProvider = cameraProvider
+            ?: throw IllegalStateException("Camera initialization failed.")
+
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(cameraFacing).build()
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview = Preview.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+            .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+            .build()
+
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder().setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(backgroundExecutor) { image ->
+                        detectHand(image)
+                    }
+                }
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(
+                this, cameraSelector, preview, imageAnalyzer
+            )
+
+            // Attach the viewfinder's surface provider to preview use case
+            preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun detectHand(imageProxy: ImageProxy) {
+        handLandmarkerHelper.detectLiveStream(
+            imageProxy = imageProxy,
+            isFrontCamera = cameraFacing == CameraSelector.LENS_FACING_FRONT
+        )
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        imageAnalyzer?.targetRotation =
+            fragmentCameraBinding.viewFinder.display.rotation
+    }
+
     companion object {
-        private const val TAG = "CameraXApp"
+        private const val TAG = "Hand Landmarker"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private val REQUIRED_PERMISSIONS =
             mutableListOf (
@@ -138,24 +276,64 @@ class CameraFragment : Fragment(), HandLandmarkerHelper.LandmarkerListener {
             }.toTypedArray()
     }
 
-//    private val activityResultLauncher =
-//        registerForActivityResult(
-//            ActivityResultContracts.RequestMultiplePermissions())
-//        { permissions ->
-//            // Handle Permission granted/rejected
-//            var permissionGranted = true
-//            permissions.entries.forEach {
-//                if (it.key in REQUIRED_PERMISSIONS && it.value == false)
-//                    permissionGranted = false
-//            }
-//            if (!permissionGranted) {
-//                Toast.makeText(baseContext,
-//                    "Permission request denied",
-//                    Toast.LENGTH_SHORT).show()
-//            } else {
-//                startCamera()
-//            }
-//        }
+    private fun transmitPoints(){
+        Log.d("TESTING", "WHAT YHE FUCK")
+        val volleyQueue = Volley.newRequestQueue(getActivity()?.getApplicationContext())
+
+
+        val jsonArray = JSONArray()
+
+        // Iterate through the 2D array and add each row to the JSON array
+        for (row in poseArray) {
+            val jsonRow = JSONArray()
+            for (element in row) {
+                jsonRow.put(element)
+            }
+            jsonArray.put(jsonRow)
+        }
+
+        // Create a JSON object to hold the JSON array
+        val jsonObject = JSONObject()
+        jsonObject.put("data", jsonArray)
+
+        // Print the JSON object
+        println(jsonObject.toString())
+        val url = "http://127.0.0.1:5000/pose"
+        val jsonObjectRequest = JsonObjectRequest(
+            // we are using GET HTTP request method
+            Request.Method.POST,
+            // url we want to send the HTTP request to
+            url,
+            // this parameter is used to send a JSON object
+            // to the server, since this is not required in
+            // our case, we are keeping it `null`
+            jsonObject,
+
+            // lambda function for handling the case
+            // when the HTTP request succeeds
+            { response ->
+                // get the image url from the JSON object
+                val msg = response.get("pose_data")
+                //TODO: handle displaying of translations on screen
+                Log.d("RES MSG", "message: " + msg)
+                // load the image into the ImageView using Glide.
+
+            },
+
+            // lambda function for handling the
+            // case when the HTTP request fails
+            { error ->
+                // make a Toast telling the user
+                // that something went wrong
+
+                // log the error message in the error stream
+                Log.e("MainActivity", "loadDogImage error: ${error.message}")
+            }
+        )
+        volleyQueue.add(jsonObjectRequest)
+        poseArray.clear()
+
+    }
 
     override fun onError(error: String, errorCode: Int) {
 //        activity?.runOnUiThread {
@@ -168,25 +346,50 @@ class CameraFragment : Fragment(), HandLandmarkerHelper.LandmarkerListener {
 //        }
     }
 
+    // Update UI after hand have been detected. Extracts original
+    // image height/width to scale and place the landmarks properly through
+    // OverlayView
+    // Update UI after hand have been detected. Extracts original
+    // image height/width to scale and place the landmarks properly through
+    // OverlayView
     override fun onResults(
         resultBundle: HandLandmarkerHelper.ResultBundle
     ) {
-//        activity?.runOnUiThread {
-//            if (_fragmentCameraBinding != null) {
-//                fragmentCameraBinding.bottomSheetLayout.inferenceTimeVal.text =
-//                    String.format("%d ms", resultBundle.inferenceTime)
-//
-//                // Pass necessary information to OverlayView for drawing on the canvas
-//                fragmentCameraBinding.overlay.setResults(
-//                    resultBundle.results.first(),
-//                    resultBundle.inputImageHeight,
-//                    resultBundle.inputImageWidth,
-//                    RunningMode.LIVE_STREAM
-//                )
-//
-//                // Force a redraw
-//                fragmentCameraBinding.overlay.invalidate()
-//            }
-//        }
+        activity?.runOnUiThread {
+            if (_fragmentCameraBinding != null) {
+
+
+                // Pass necessary information to OverlayView for drawing on the canvas
+                fragmentCameraBinding.overlay.setResults(
+                    resultBundle.results.first(),
+                    resultBundle.inputImageHeight,
+                    resultBundle.inputImageWidth,
+                    RunningMode.LIVE_STREAM
+                )
+
+                // Force a redraw
+                fragmentCameraBinding.overlay.invalidate()
+            }
+        }
+        if(transmit){
+            for (landmark in resultBundle.results){
+                for( landmarkRes in landmark.landmarks()){
+                    val floatArray = FloatArray(126)
+                    var j=0
+                    for ( landmarkCoord in landmarkRes){
+
+                        Log.d("POSE ESTIMATION", "X:" + landmarkCoord.x() + "Y:" + landmarkCoord.y() + "Z:" + landmarkCoord.z())
+                        floatArray[j] = landmarkCoord.x()
+                        floatArray[j+1] = landmarkCoord.y()
+                        floatArray[j+2] = landmarkCoord.z()
+                        j+=3
+                    }
+                    poseArray.add(floatArray)
+
+                }
+            }
+        }
+
+
     }
 }
